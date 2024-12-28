18d648da1ebf5061a3c09c827334e2c747450cc2
NICOHELENA <159774510+NICOHELENA@users.noreply.github.com>
2024-12-28 13:15:34 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP KADAI_011</title>
+ </head>
+  
+  
+<body>
+<?php
+$product = ['名前' => '玉ねぎ', '値段' => 200,  '産地' => '北海道'];
+
+foreach ($product as $key => $value) {
+    echo "{$key}:{$value}<br>";
+}
+
+?>
+</body>
+</html>
